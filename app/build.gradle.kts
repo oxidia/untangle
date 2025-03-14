6d8d16d4ae2910f063a2547c7b14377dcd72c025
Youssef Bahlaoui <youssef-bahlaoui@outlook.fr>
2025-03-14 23:11:29 +0000

    chore: install dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    // ksp
+    alias(libs.plugins.kotlin.devtools.ksp)
+
+    // dagger hilt
+    alias(libs.plugins.android.dagger.hilt)
 }
 
 android {
@@ -56,4 +62,18 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // coroutines
+    implementation(libs.kotlinx.coroutines.android)
+
+    // dagger hilt
+    implementation(libs.hilt.android)
+    implementation(libs.androidx.hilt.navigation.compose)
+    ksp(libs.hilt.android.compiler)
+
+    // retrofit2
+    implementation(libs.retrofit)
+    // gson
+    implementation(libs.gson)
+    implementation(libs.converter.gson)
 }
